0609f9ad909566bcde2e66a38ca8b12f7f7b8457
Jiyoung Yoon <56244911+ebbuni1023@users.noreply.github.com>
2021-07-22 00:35:23 -0500

    firebase

---
+++ config/firebase.js
@@ -0,0 +1,13 @@
+
+export default {
+    firebaseConfig: {
+        apiKey: "AIzaSyDHnq37Wszx7hBMBiMDD5grN3VChZ7Q7Hw",
+        authDomain: "nowaste-9ddd0.firebaseapp.com",
+        databaseURL: "https://nowaste-9ddd0-default-rtdb.firebaseio.com",
+        projectId: "nowaste-9ddd0",
+        storageBucket: "nowaste-9ddd0.appspot.com",
+        messagingSenderId: "678683026925",
+        appId: "1:678683026925:web:df3709ca219817a2e3f3e4",
+        measurementId: "G-4XZY64H547"
+    }
+  }
