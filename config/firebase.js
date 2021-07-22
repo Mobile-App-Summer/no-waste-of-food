c5de61a30794a30650c06b3e63a934d7daaf0c7e
Jiyoung Yoon <56244911+ebbuni1023@users.noreply.github.com>
2021-07-22 02:51:44 -0500

    login done

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import Constants from 'expo-constants';
 
-export default {
-    firebaseConfig: {
-        apiKey: "AIzaSyDHnq37Wszx7hBMBiMDD5grN3VChZ7Q7Hw",
-        authDomain: "nowaste-9ddd0.firebaseapp.com",
-        databaseURL: "https://nowaste-9ddd0-default-rtdb.firebaseio.com",
-        projectId: "nowaste-9ddd0",
-        storageBucket: "nowaste-9ddd0.appspot.com",
-        messagingSenderId: "678683026925",
-        appId: "1:678683026925:web:df3709ca219817a2e3f3e4",
-        measurementId: "G-4XZY64H547"
-    }
-  }
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDHnq37Wszx7hBMBiMDD5grN3VChZ7Q7Hw",
+  authDomain: "nowaste-9ddd0.firebaseapp.com",
+  databaseURL: "https://nowaste-9ddd0-default-rtdb.firebaseio.com",
+  projectId: "nowaste-9ddd0",
+  storageBucket: "nowaste-9ddd0.appspot.com",
+  messagingSenderId: "678683026925",
+  appId: "1:678683026925:web:df3709ca219817a2e3f3e4",
+  measurementId: "G-4XZY64H547"
+};
+
+let Firebase;
+
+if (firebase.apps.length === 0) {
+  Firebase = firebase.initializeApp(firebaseConfig);
+}
+
+export default Firebase;
